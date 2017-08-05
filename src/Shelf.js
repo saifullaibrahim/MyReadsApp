0c00599aa23a82dcac78f2c6575c55739726d59d
Vipul Masurkar <masurkarvipul@gmail.com>
2017-08-06 03:57:29 +0530

    feat: Refactor the starter code to react
    More detailed explanatory text, the starter boilerplate had
    some initial code wrriten in it, refactor that to use react features
    change static contents to react components
    make the application functional as per the rubric
    refactor to improve the code

---
+++ src/Shelf.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import {Component} from 'react'
+import * as BooksAPI from './BooksAPI'
+import Book from './Book'
+
+import {Link} from 'react-router-dom'
+
+class Shelf extends Component {
+  state = {
+    books: []
+  }
+  getAllBooks = () => {
+    BooksAPI.getAll().then((books) => {
+      this.setState(state => ({books: books}))
+    })
+  }
+  componentWillMount() {
+    this.getAllBooks()
+  }
+
+  handleChange = (book, shelf) => {
+    (shelf !== "none") && BooksAPI.update(book, shelf).then(() => {
+      this.getAllBooks()
+    })
+  }
+  render() {
+    const booksData = this.state.books
+    return (
+      <div className="app">
+        <div className="list-books">
+          <div className="list-books-title">
+            <h1>My Reads</h1>
+          </div>
+
+          <div className="list-books-content">
+            <div>
+              <div className="bookshelf">
+                <h2 className="bookshelf-title">Currently Reading</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    {booksData.filter((book) => {
+                      return book.shelf === "currentlyReading"
+                    }).map((currentlyReading) => (
+                      <li key={currentlyReading.id}>
+                        <Book book={currentlyReading} changeState={this.handleChange}/>
+                      </li>
+                    ))}
+                  </ol>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="bookshelf">
+                <h2 className="bookshelf-title">Want to Read</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    {booksData.filter((book) => {
+                      return book.shelf === "wantToRead"
+                    }).map((wantToRead) => (
+                      <li key={wantToRead.id}>
+                        <Book book={wantToRead} changeState={this.handleChange}/>
+                      </li>
+                    ))}
+                  </ol>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="bookshelf">
+                <h2 className="bookshelf-title">Read</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    {booksData.filter((book) => {
+                      return book.shelf === "read"
+                    }).map((read) => (
+                      <li key={read.id}>
+                        <Book book={read} changeState={this.handleChange}/>
+                      </li>
+                    ))}
+                  </ol>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="open-search">
+            <Link className="add-contact" to="/search">Add a book</Link>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Shelf
